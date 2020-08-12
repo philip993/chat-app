54ba604fbfe7f145eb2912af9ffca44d250f1215
Filip Mihajlovski <fitz993m@gmail.com>
2020-08-12 11:26:39 +0200

    1.8. Finished

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,27 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const usersList = document.getElementById("users");
+
+// GET username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const socket = io();
 
+// Join chatroom
+socket.emit("joinRoom", { username, room });
+
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
 // Message from server
-socket.on("message", (mesage) => {
-  console.log(mesage);
+socket.on("message", (message) => {
+  console.log(message);
   outputMessage(message);
 
   // Scroll donw
@@ -27,11 +43,29 @@ chatForm.addEventListener("submit", (e) => {
 
 // Outpust message to DOM
 function outputMessage(message) {
-  const div = document.createElement("div");
-  div.classList.add("message");
-  div.innerHTML = `<p class='meta'>Filip <span>11:12 pm </span></p>
-    <p class='text'>
-    ${message}
-    </p>`;
-  document.querySelector(".chat-messages").appendChild(div);
+  const something = document.createElement("div");
+  something.classList.add("message");
+  something.innerHTML += `  <p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+   ${message.text}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(something);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  usersList.innerHTML = `
+    ${users
+      .map(
+        (user) => `
+    <li>${user.username}</li>
+    `
+      )
+      .join("")}
+  `;
 }
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,23 +17,55 @@ const io = socketio(server);
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatApp Bot";
+
 // Run when client connects
 io.on("connection", (socket) => {
-  // Welcome current user
-  socket.emit("message", "Welcome to Chat Cord App");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to Chat Cord App"));
 
-  // Broadcast when user connects - Admin
-  socket.broadcast.emit("message", "A user has joined the chat.");
+    // Broadcast when user connects - Admin
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `A ${user.username} has joined the chat.`)
+      );
 
-  // Runs when clients disconnect
-  socket.on("disconnect", () => {
-    io.emit("message", "User has left the chat!");
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   // Listen for chat message
   socket.on("chatMessage", (msg) => {
-    io.emit("chatMessage", msg);
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("chatMessage", formatMessage(user.username, msg));
     console.log(msg);
+  });
+
+  // Runs when clients disconnect
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat!`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
   });
 });
 
